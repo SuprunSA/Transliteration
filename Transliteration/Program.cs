ac0aab49767cfcf7de47687aa4def165247b45a1
Супрун Сергей <4everyongg@mail.ru>
2021-10-08 22:10:07 +0400

    Requuired

--- Transliteration/Program.cs
+++ Transliteration/Program.cs
@@ -1,11 +1,14 @@
 ﻿using static System.Console;
 using System.Text;
+using System;
 
 namespace Transliteration
 {
     class Program
     {
+        public static int currTranslitIndex = 0;
         static string input = "";
+
         static char[,] trRule = { { 'б', 'b'}, 
             { 'в', 'v' }, 
             { 'г', 'g' },
@@ -38,20 +41,28 @@ namespace Transliteration
 
         static void Main(string[] args)
         {
-            ReadText();
-            Translit();
+            DoTranslit();
         }
 
-        static void Translit()
+        static void DoTranslit()
         {
-            StringBuilder sb = new StringBuilder(input);
-            for (int i = 0; i < trRule.GetUpperBound(0); i++)
+            do
             {
-                sb.Replace(trRule[i, 0], trRule[i, 1]);
-                sb.Replace(trRule[i, 0].ToString().ToUpper(), trRule[i, 1].ToString().ToUpper());
+                FindIndex(currTranslitIndex);
+                WriteLine("Нажмите любую клавишу, чтобы продолжить, или нажмите для выхода Esc ");
+            } while (ReadKey().Key != ConsoleKey.Escape);
+        }
+        static void Translit(int index)
+        {
+            WriteLine();
+            StringBuilder sb = new StringBuilder(input);
+            sb.Replace(firmSign[index], firmSign[Math.Abs(index - 1)]);
+            sb.Replace(firmSign[index].ToUpper(), firmSign[Math.Abs(index - 1)]);
+            for (int i = 0; i < trRule.GetUpperBound(0) + 1; i++)
+            {
+                sb.Replace(trRule[i, index], trRule[i, Math.Abs(index - 1)]);
+                sb.Replace(trRule[i, index].ToString().ToUpper(), trRule[i, Math.Abs(index - 1)].ToString().ToUpper());
             }
-            sb.Replace(firmSign[0], firmSign[1]);
-            sb.Replace(firmSign[0].ToUpper(), firmSign[1]);
             WriteLine(sb);
         }
 
@@ -60,6 +71,29 @@ namespace Transliteration
             WriteLine("Введите текст для транслитерации: ");
             input = ReadLine();
             return input;
+        }
+
+        static int FindIndex(int index)
+        {
+            Clear();
+            WriteLine("Введите F1 для транслитерации кириллицы в латинницу или F2 для транслитерации латинницы в кириллицу ");
+            var key = ReadKey();
+            switch (key.Key)
+            {
+                case ConsoleKey.F1:
+                    index = 0;
+                    Clear();
+                    ReadText();
+                    Translit(currTranslitIndex);
+                    break;
+                default:
+                    index = 1;
+                    Clear();
+                    ReadText();
+                    Translit(currTranslitIndex);
+                    break;
+            }
+            return index;
         }
     }
 }
